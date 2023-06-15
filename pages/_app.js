07bd54ad6361b2d215eacd4904e412cf7dde68ec
Achmad Maulana <achmad.maulana@big-solution.net>
2023-06-15 17:23:30 +0700

    updated

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,31 @@
 import { ThemeProvider } from "next-themes";
 import "../css/tailwind.css";
+import { useEffect } from "react"
 
 function MyApp({ Component, pageProps }) {
+
+  useEffect(() => {
+    if("serviceWorker" in navigator) {
+      window.addEventListener("load", function () {
+       navigator.serviceWorker.register("service-worker.js").then(
+          function (registration) {
+            console.log("Service Worker registration successful with scope: ", registration.scope);
+          },
+          function (err) {
+            console.log("Service Worker registration failed: ", err);
+          }
+        );
+      });
+    }
+  }, [])
+
   return (
     <ThemeProvider attribute="class">
       <Component {...pageProps} />
     </ThemeProvider>
   );
 }
-
+// self.addEventListener("install", function (event) {
+//   console.log("Hello world from the Service Worker 🤙");
+// });
 export default MyApp;
--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,8 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400..700&display=swap"
             rel="stylesheet"
           />
+          <Link rel="manifest" href="/manifest.json" />
+
         </Head>
         <body>
           <Main />
